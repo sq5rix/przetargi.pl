867cc2432750fd667415bad42f1327f9d6e2610e
Tom <sq5rix@gmail.com>
2016-05-09 03:13:21 +0200

    some result - dictionary with keys and lists of results

--- przetargi.py
+++ przetargi.py
@@ -14,24 +14,31 @@ else:
 quest = re.sub('\s', '+', srch)
 print(quest)
 url = 'http://www.przetargi.egospodarka.pl/search.php?submitted=1&phrase=' + quest
+
 headers = {}
 headers['User-Agent'] = \
     "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-try:
-    req = Request(url, headers=headers)
-    resp = urlopen(req)
-    respData = resp.read()
-except:
-    pass
 
-soup = BeautifulSoup(respData, "lxml")
+req = Request(url, headers=headers)
+resp = urlopen(req)
+resp_data = resp.read()
+
+soup = BeautifulSoup(resp_data, "lxml")
 data = {}
 for link in soup.find_all('td'):
     key = link.get('data-th')
-    if 'Daty' in key:
-        end_date = link.text.partition('\n\n')[2]
-        data[key] = end_date.partition(' ')[0]
+    if key in data:
+        if 'Daty' in key:
+            end_date = link.text.partition('\n\n')[2]
+            data[key].append(end_date.partition(' ')[0])
+        else:
+            data[key].append(link.find('a').text)
     else:
-        data[key] = link.find('a').text
+        if 'Daty' in key:
+            end_date = link.text.partition('\n\n')[2]
+            data[key] = [end_date.partition(' ')[0]]
+        else:
+            data[key] = [link.find('a').text]
 
 print(data)
+
