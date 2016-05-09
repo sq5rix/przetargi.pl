fe05db71be3d8827a27aa4cb3c6e51fa45f64b41
Tom <sq5rix@gmail.com>
2016-05-09 02:29:55 +0200

    first try

---
+++ przetargi.py
@@ -0,0 +1,37 @@
+import sys
+import re
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+
+srch = ''
+if len(sys.argv) >= 2:
+    for i in range(1, len(sys.argv)):
+        srch += sys.argv[i] + ' '
+    srch = srch[:-1]
+else:
+    srch = input("What do you look for? ")
+
+quest = re.sub('\s', '+', srch)
+print(quest)
+url = 'http://www.przetargi.egospodarka.pl/search.php?submitted=1&phrase=' + quest
+headers = {}
+headers['User-Agent'] = \
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+try:
+    req = Request(url, headers=headers)
+    resp = urlopen(req)
+    respData = resp.read()
+except:
+    pass
+
+soup = BeautifulSoup(respData, "lxml")
+data = {}
+for link in soup.find_all('td'):
+    key = link.get('data-th')
+    if 'Daty' in key:
+        end_date = link.text.partition('\n\n')[2]
+        data[key] = end_date.partition(' ')[0]
+    else:
+        data[key] = link.find('a').text
+
+print(data)
